b5460e8be6d24b06347cb7cb1429fcd340720623
EC2 Default User <ec2-user@ip-172-31-17-166.ap-northeast-1.compute.internal>
2023-05-17 02:55:05 +0000

    show-Qgo

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,9 +1,9 @@
 class BooksController < ApplicationController
   def new
-    @book = Book.new
   end
 
   def index
+    @book = Book.new
     @books = Book.all
   end
 
@@ -13,31 +13,16 @@ class BooksController < ApplicationController
     # 3. データをデータベースに保存するためのsaveメソッド実行
     book.save
     # 4. 画面へリダイレクト先
-    redirect_to 'books'
+    redirect_to book_path(book.id)
   end
 
   def show
-     @book = Book.find(params[:id])
+    @book = Book.find(params[:id])
   end
 
   def edit
-    # @book = Book.find(params[:id])
+    @book = Book.find(params[:id])
   end
-
-  #def update
-    #@book = Book.find(params[:id]) # 与えられたIDを持つ本を検索するためにコードを追加します。
-    #if @book.update(book_params)
-      #redirect_to books_path
-    #else
-      #render 'edit'
-    #end
-  #end
-
-  #def destroy
-    #@book = Book.find(params[:id]) # 与えられたIDを持つ本を検索するためにコードを追加します。
-    #@book.destroy
-    #redirect_to books_path
-  #end
 
   private
   def book_params
